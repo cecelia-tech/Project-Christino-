8051f3ca18ff6dc778a5bd8abe37bb3e49e2dc30
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-03-29 22:35:57 +0300

    Structure

---
+++ js/mainHome.js
@@ -0,0 +1,21 @@
+// IMPORT
+// header
+// services
+// achievements
+//featured projects
+//feedback
+//price list
+//blog
+//icons
+// footer
+
+// FUNKCIJU PANAUDOJIMAS
+// header
+// services
+// achievements
+//featured projects
+//feedback
+//price list
+//blog
+//icons
+// footer
