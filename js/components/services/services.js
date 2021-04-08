8ecd84a637ac705a3492b7bcf911a558bdbf6259
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-04-08 09:16:12 +0300

    service item validations #18

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -1,17 +1,40 @@
+import {isValidServices} from './isValidServices.js';
+import {isValidServiceItem} from './isValidServiceItem.js';
+
+
 function services(selector, data) {
     //imput validation
 
+    if (!isValidServices(selector, data)) {
+        return false;
+    }
+
     //logic
     const DOM = document.querySelector(selector);
-    const {list} = data;
+    if (!DOM) {
+        console.error('Pagal pateikta selektoriu nerastas norimas elementas');
+        return false;
+    }
+    const {list, maxCount} = data;
    // const servicesArray = data.list;
 
     let HTML ='';
+    let generatedServicesCount = 0;
     for (let i = 0; i < list.length; i++) {
         const service = list[i];
+
+        //service iyem validation
+        if (!isValidServiceItem(service)) {
+            continue;
+        }
+        // galima abu situs itemus apjungt
         if (!service.active) {
             continue;
         }
+        if (generatedServicesCount === maxCount) {
+            break;
+        }
+        generatedServicesCount++;
         HTML += `<div class="col-12 col-md-6 col-lg-4 services center"> 
                 ${service.icon}
                 <p class="mini-title center">${service.title}</p>
