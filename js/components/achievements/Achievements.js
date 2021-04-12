da600a177f3f34e7d23a2af1a10476da1ce592ee
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-04-13 00:50:04 +0300

    Scroll event added #23

--- js/components/achievements/Achievements.js
+++ js/components/achievements/Achievements.js
@@ -43,8 +43,9 @@ class Achievements {
         }
 
         this.DOM = DOM;
-
+        
         this.render();
+        this.addEvent();
     }
 
     isValidSelector() {
@@ -85,15 +86,60 @@ class Achievements {
             ${item.icon}
             <p class="numbers">0</p>
         <p class="text"> ${item.text}</p></div>`;
-        console.log(HTML);
         }
         for (let i = 0; i < this.data.list.length; i++) {
             const item = this.data.list[i];
             
         }
         this.DOM.innerHTML = HTML;
+        
     }
-    
+    addEvent() {
+        addEventListener('scroll', () => { 
+            const allNumbersDOM = this.DOM.querySelectorAll('.numbers');
+
+
+            for (let i = 0; i < allNumbersDOM.length; i++) {
+                const numberDOM = allNumbersDOM[i];
+                const elementTop = numberDOM.offsetTop;
+                const elementHeight = numberDOM.clientHeight;
+
+                const isVisible = scrollY + innerHeight >= elementTop + elementHeight;
+                if (isVisible) {
+                    this.animateNumber(numberDOM,i);
+                }
+            }
+        })
+    }
+    animateNumber(elementDOM, elementIndex){
+        //ar element.animater === true
+        if (this.data.list[elementIndex].animated !== true) {
+            //priesingu atveju - animuoju ir pazymiu, jog jau suanimuota
+            const targetNumber = this.data.list[elementIndex].value;
+            this.data.list[elementIndex].animated = true;
+
+            const timeToAnimate = 3000;  //ms
+            const fps = 30;             //kartai per sekunde
+            const framesCount = timeToAnimate *fps / 1000; //kiek is viso kadru          
+            const numberIncrement = targetNumber / framesCount;
+            let printedValue = 0;
+            let currentFrameIndex = 0;
+            const timer = setInterval(() => {
+                printedValue += numberIncrement;
+                currentFrameIndex++;
+                elementDOM.innerText = Math.round(printedValue);
+                if (currentFrameIndex === framesCount) {
+                    clearInterval(timer)
+                }
+            }, 1000 / fps )
+            //setTimeout (func, time to wait in ms)
+            // setTimeout(() => {
+            //     elementDOM.innerText = targetNumber;
+            // }, 2000)
+
+        }
+        
+    }
 }
 
 export { Achievements};
